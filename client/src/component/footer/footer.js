72d635515bebf5b43de2b6bd1d0a40af34aaafc3
BachNd <nguyenduybach96@gmail.com>
2020-12-14 13:16:14 +0700

    change footer

--- client/src/component/footer/footer.js
+++ client/src/component/footer/footer.js
@@ -39,13 +39,13 @@ class Footer extends Component {
                                     <p>Chủ nhật <span>09 AM - 19 PM</span></p>
                                 </div>
                                 <div className="col-md-12 col-sm-12 col-xs-12  Footer-infor-hourwork">
-                                    <h6><img src={img_phone_call} alt="cty"/><span> (+84 - 028) 38964922</span></h6>
+                                    <h6><img src={img_phone_call} alt="cty"/><span><a href="tel:0412345678"> (+84 - 04) 12345678</a></span></h6>
                                 </div>
                                 <div className="col-md-12 col-sm-12 col-xs-12  Footer-infor-hourwork">
-                                    <h6><img src={img_envelope} alt="cty"/><span>ptchc@hcmute.edu.vn</span> </h6>
+                                    <h6><img src={img_envelope} alt="cty"/><span><a href="mailto:mindx.edu.vn">mindx.edu.vn</a></span> </h6>
                                 </div>
                                 <div className="col-md-12 col-sm-12 col-xs-12  Footer-infor-hourwork">
-                                    <h6><img src={img_location} alt="cty"/> <span>1 Võ Văn Ngân, Phường Linh Chiểu, Quận Thủ Đức, Thành phố Hồ Chí Minh.</span></h6>
+                                    <h6><img src={img_location} alt="cty"/><span><a href="https://www.google.com/maps/place/Th%C3%A0nh+C%C3%B4ng,+Ba+%C4%90%C3%ACnh,+H%C3%A0+N%E1%BB%99i/data=!4m2!3m1!1s0x3135ab6516def4cf:0x195ed79b219030e7?sa=X&ved=2ahUKEwi2xNjg5sztAhWDMN4KHft7B9sQ8gEwC3oECBQQAQ" target="_blank">Thành Công, Ba Đình, Hà NộiNội</a></span> </h6>
                                 </div>
                             
                             </div>
@@ -87,7 +87,7 @@ class Footer extends Component {
                     
                 </div>
                 <div className="Copyright">
-                        <p>© Bản quyền thuộc về Phát Đoàn | Cung cấp bởi Phát Đoàn và Việt Văn</p>
+                        <p>New</p>
                 </div>
          </div>
         );
